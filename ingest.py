37ba16e144df2535e1ffa27b3adc96ddf6b97a43
saimanasreen001 <saima.nasreen@jktech.com>
2025-07-10 03:36:27 -0700

    completed ingestion and storage

---
+++ ingest.py
@@ -0,0 +1,26 @@
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader # used to load and parse documents
+from langchain.text_splitter import RecursiveCharacterTextSplitter # create chunks along with text overlapping
+from langchain_huggingface import HuggingFaceEmbeddings #HuggingFaceEmbeddings class is used to generate embeddings
+from langchain_community.vectorstores import FAISS #FAISS is used to store embeddings
+
+DATA_PATH = 'data/' # stores the pdf from the data folder
+DB_FAISS_PATH = 'vectorstore/db_faiss' # embeddings are stored inside vector store folder which 
+                                        # is generated after ingest.py runs
+
+#function to create vector_db
+def create_vector_db():
+
+    #DirectoryLoader loads all files from a folder.
+    #PyPDF loads a single PDF file.
+    loader = DirectoryLoader(DATA_PATH, glob='*.pdf',loader_cls=PyPDFLoader)
+
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    texts = text_splitter.split_documents(documents)
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
+                                       model_kwargs={'device':'cpu'}) # embeddings are created
+    db=FAISS.from_documents(texts, embeddings) # creates a FAISS vector store
+    db.save_local(DB_FAISS_PATH) # saves the folder locally. Path is stored inside DB_FAISS_PATH
+
+if __name__ == "__main__":
+    create_vector_db()
